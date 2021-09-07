21c3a7d462eb0d0c0b7977671ec36805b4bbca4d
EdikxXx <edekx2011@gmail.com>
2021-09-07 11:28:13 +0300

    Comment
    C# библиотека, которая умеет вычислять площадь круга по радиусу и треугольника по трем сторонам.

---
+++ CalculatingArea/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalculatingArea
+{
+    public class Area
+    {
+
+        public double Circle_area(double R)
+        {
+            double S= Math.PI * Math.Pow(R,2) ;
+            return S;
+        }
+
+        public double Triangle_area (double a, double b, double c)
+        {
+            double p=(a+b+c)/2;
+            double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return S;
+        }
+
+    }
+}
